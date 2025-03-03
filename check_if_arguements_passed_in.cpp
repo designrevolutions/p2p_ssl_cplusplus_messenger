9acbae177756070028563dc3205944d875f7f119
Omar Miah <omar@designrevolutions.com>
2025-03-03 12:22:07 +0000

    Added code to check for how many args passed in. I wanted to modify the code and add ability to tun the code on the command line intead of question prompts and input - this makes testing the code easier.

---
+++ check_if_arguements_passed_in.cpp
@@ -0,0 +1,33 @@
+// This code is C++ code to check if any arguements were passed in on the command prompt
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    // argc is the number of arguments passed in - this includes the file name itself
+
+    std::cout << "Number of arguments passed: " << argc - 1 << std::endl;
+    std::cout << std::endl
+              << std::endl;
+
+    int arguements_passed_in = argc - 1;
+
+    if (arguements_passed_in == 0)
+    {
+        std::cout << "No arguments passed in." << std::endl;
+    }
+    else if (arguements_passed_in == 2)
+    {
+        std::cout << "TWO argument passed in." << std::endl;
+    }
+    else
+    {
+        std::cout << "We're going into the loop now... arguments passed in:" << std::endl;
+        for (int i = 1; i < argc; i++)
+        {
+            std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+        }
+    }
+
+    return 0;
+}
